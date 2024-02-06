403a148631207ebec666c088a7431147005057d0
maborosii <bonvovageonepiece@gmail.com>
2024-02-06 11:34:31 +0800

    init

---
+++ 0001_two_sum/main.rs
@@ -0,0 +1,29 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        use std::collections::HashMap;
+        let mut a: Vec<i32> = vec![];
+        let m_h: HashMap<_, _> = nums
+            .iter()
+            .enumerate()
+            .map(|(index, &value)| (value, index))
+            .collect();
+
+        for (nums_i, nums_v) in nums.iter().enumerate() {
+            let o = &target - nums_v;
+            match m_h.get(&o) {
+                Some(&ii) => {
+                    if ii == nums_i {
+                        continue;
+                    }
+                    a.push(nums_i as i32);
+                    a.push(ii as i32);
+                    break;
+                }
+                None => {
+                    continue;
+                }
+            }
+        }
+        a
+    }
+}
